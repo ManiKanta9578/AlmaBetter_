2fbd71e210a83226ffe04bbeb06fa4b05071792f
ManiKanta <manikanta9578@gmail.com>
2022-09-06 12:04:44 +0530

    JS_Intro

---
+++ JS_Intro.js
@@ -0,0 +1,10 @@
+//Q1 Print Hello World! in the console below.
+console.log("Hello World!");
+
+//Q3 Print the input specified: input = "This is the first and only line of input string".
+var input = "This is the first and only line of input string";
+console.log(input);
+
+//Q4 Using javascript variables, declare a variable named newVariable and assign it the following string, "AlmaBetter". 
+var newVariable = "AlmaBetter";
+console.log(newVariable);
