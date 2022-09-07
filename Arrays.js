0028a44ee4c52195eb89452bf442eee3c32b321d
ManiKanta <manikanta9578@gmail.com>
2022-09-07 12:37:29 +0530

    Arrays practice

---
+++ Arrays.js
@@ -0,0 +1,25 @@
+/*
+const heros = [ { name: 'Spider-Man' }, 
+{ name: 'Thor' }, 
+{ name: 'Black Panther' }, 
+{ name: 'Captain Marvel' }, 
+{ name: 'Silver Surfer' } 
+];
+const arr = heros.map((hero,index) =>{
+    return {id:index, hero:hero.name}
+})
+console.log(arr);
+*/
+
+/*
+let myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.join());
+console.log(myColor.join('+'));
+*/
+
+/*
+let strings = ["avengers", "captain america", "ironman", "black panther"];
+console.log(strings.map(string => string.toUpperCase()));
+*/
+
+
