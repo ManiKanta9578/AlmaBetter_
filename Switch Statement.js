15bbe1afdd736f5571f5b482b14959c49f9720f5
ManiKanta <manikanta9578@gmail.com>
2022-09-09 17:09:26 +0530

    switch statement

---
+++ Switch Statement.js
@@ -0,0 +1,17 @@
+let fruit = prompt("Know your color");
+switch(fruit){
+    case "apple":
+        console.log(`${fruit} is red color` );
+        break;
+    case "orange":
+        console.log(`${fruit} is orange color` );
+        break;
+    case "mango":
+        console.log(`${fruit} is yellow color` );
+        break;
+    case "grapes":
+        console.log(`${fruit} is green color` );
+        break;
+    default:
+        console.log(`${fruit} is not a fruit`);
+}
