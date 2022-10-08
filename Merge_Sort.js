210e91cc727427f7cf9d25754ac2b3ac134ab854
manikanta9578 <manikanta9578@gmail.com>
2022-10-08 17:28:41 +0530

    mergeSort

--- Merge_Sort.js
+++ Merge_Sort.js
@@ -1,53 +1,74 @@
-let array=[34,21,41,12,1,6,89];
-console.log(array);
+function merge(arr, l, m, r)
+{
+	let n1 = m - l + 1;
+	let n2 = r - m;
 
-mergesort(array,0,array.length-1);
-console.log(array);
+	// Create temp arrays
+	let L = new Array(n1);
+	let R = new Array(n2);
 
-function mergesort(arr,left,right){
-    if(left<right){
-        let mid=parseInt(left+(right-left)/2);
-        mergesort(arr,left,mid);
-        mergesort(arr,mid+1,right);
-        merge(arr,left,mid,right);
-    }
+	// Copy data to temp arrays L[] and R[]
+	for (let i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (let j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+
+	// Merge the temp arrays back into arr[l..r]
+
+	// Initial index of first subarray
+	let i = 0;
+
+	// Initial index of second subarray
+	let j = 0;
+
+	// Initial index of merged subarray
+	let k = l;
+
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	// Copy the remaining elements of
+	// L[], if there are any
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	// Copy the remaining elements of
+	// R[], if there are any
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
 }
 
-
-function merge(arr,left,mid,right){
-    let m=mid-left+1;
-    let n=right-mid;
-
-    let leftArray=[];
-    let rightArray=[];
-
-    for(let i=0;i<m;i++){
-        leftArray[i]=arr[left+i];
-    }
-
-    for(let i=0;i<n;i++){
-        rightArray[i]=arr[mid+i+1];
-    }
-    let i=0,j=0,k=left;
-
-    while(i<m && j<n){
-        if(leftArray[i]<=rightArray[j]){
-            arr[k]=leftArray[i];
-            i++;
-            k++;
-        }
-        else{
-            arr[k]=rightArray[j];
-            j++;
-            k++;
-        }
-    }
-
-    while(i<m){
-        arr[k]=leftArray[i]; i++; k++;
-    }
-
-    while(j<n){
-        arr[k]=rightArray[j]; j++;k++;
-    }
+// l is for left index and r is
+// right index of the sub-array
+// of arr to be sorted */
+function mergeSort(arr,l, r){
+	if(l>=r){
+		return;//returns recursively
+	}
+	let m =l+ parseInt((r-l)/2);
+	mergeSort(arr,l,m);
+	mergeSort(arr,m+1,r);
+	merge(arr,l,m,r);
 }
+
+let arr = [ 12, 11, 13, 5, 6, 7 ];
+console.log(arr);	
+
+mergeSort(arr, 0, arr.length-1);
+console.log(arr);
+	
